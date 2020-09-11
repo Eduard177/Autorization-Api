b8c89a225dd411fa103556e3bacfbfc46b2850d0
Eduard <Eduarro2001@gmail.com>
2020-09-11 17:21:32 -0400

    [ADD]: DB structured

---
+++ src/database/database.service.ts
@@ -0,0 +1,29 @@
+import {ConfigService} from '../config/config.service';
+import {Configuration} from '../config/config.keys';
+import { MongooseModule } from '@nestjs/mongoose';
+import {ConfigModule} from '../config/config.module'
+
+export const databaseProviders = [
+    MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) =>({
+            uri: configService.get(Configuration.MONGO_URI),
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateIndex:true
+        }),
+        
+    }),
+    MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) =>({
+            uri: configService.get(Configuration.MONGO_URI2),
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateIndex:true
+        }),
+        
+    })
+]

852043d8305dd97f9602a7f319ef7eaedf8f2dcf
Eduard <eduarro2001@gmail.com>
2020-10-02 13:27:57 -0400

    [Add] logIn Tries added

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Patch, Post, Res } from '@nestjs/common';
+import { Body, Controller, Patch, Post} from '@nestjs/common';
 import { ForgetPasswordDTO, UserDTO } from '../dto/user.dto';
 import { AuthService } from '../auth/auth.service';
 
@@ -7,26 +7,14 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
     
     @Post('login')
-    async loginUser(@Res() res, @Body() userDTO: UserDTO) {
+    async loginUser( @Body() userDTO: UserDTO) {
       await this.authService.getAuthenticatedUser(userDTO.email, userDTO.id, userDTO.password);
-      res.status(201).json({
-        statusCode: 201,
-        message: 'User Exist',
-      });
     }
     @Post('send-email')
-    async SendEmail(@Res() res, @Body() userDTO: UserDTO) {
-      await this.authService.sendEmail(userDTO.email);
-      res.status(200).json({
-        statusCode: 200,
-        message: 'User Exist',
-      });
+    async SendEmail( @Body() forgetPasswordDTO: ForgetPasswordDTO) {
+      await this.authService.sendEmail(forgetPasswordDTO);
     }
     @Patch('password/new')
-    async forgetPassword(@Res() res, @Body() forgetPasswordDTO: ForgetPasswordDTO ){
+    async forgetPassword( @Body() forgetPasswordDTO: ForgetPasswordDTO ){
       await this.authService.forgetPassword(forgetPasswordDTO);
-      res.status(200).json({
-        statusCode:200,
-        message: 'change your passowrd'
-      })
     }}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+
 import { Model } from 'mongoose';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel} from '@nestjs/mongoose';
@@ -7,23 +8,25 @@ import { compareSync, hashSync } from 'bcrypt';
 
 import * as nodemailer from 'nodemailer';
 
+let count = 1;
 @Injectable()
 export class AuthService {
     constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
     async getAuthenticatedUser(email: string, id: number,password: string) {
         const user = await this.getByEmail(email, id);
-        await this.verifyPassword( password, user.password);
-    }
+        const passwordH = await this.verifyPassword( password, user.password);
+        await this.countTries(passwordH, email)
+      } 
     
       /////////////////////Did you forget your password?/////////////////////
     
-      async sendEmail(email: string){
-        const user = await this.userModel.findOne({ email: email });
+      async sendEmail(forgetPasswordDTO: ForgetPasswordDTO){
+        const user = await this.userModel.findOne({ email: forgetPasswordDTO.email });
         if(user){
-          await this.sendEmailTo()
+          await this.sendEmailToForget(forgetPasswordDTO)
           throw new HttpException(
-            'Send',
+           `Send Email to ${forgetPasswordDTO.email}`,
             HttpStatus.OK,
           );
         }else{
@@ -35,6 +38,12 @@ export class AuthService {
       }
       async forgetPassword(forgetPasswordDTO: ForgetPasswordDTO){
         const user = await this.userModel.findOne({ email: forgetPasswordDTO.email });
+        if(forgetPasswordDTO.newPassword == ""){
+          throw new HttpException(
+            'Password cannot be empty',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
         if(user){
             this.userModel.updateOne({email:forgetPasswordDTO.email},{
               password : hashSync(forgetPasswordDTO.newPassword,10)
@@ -46,6 +55,9 @@ export class AuthService {
                 );
               }
             })
+            throw new HttpException(
+              'Your password is changed',
+              HttpStatus.OK,)
           
         }else{
           throw new HttpException(
@@ -57,26 +69,47 @@ export class AuthService {
     
       /////////////////////Methos/////////////////////
     
-      async sendEmailTo() {
+      async sendEmailToForget(forgetPasswordDTO: ForgetPasswordDTO) {
        const transporter = nodemailer.createTransport({
           host: "smtp.ethereal.email",
           port: 587,
           secure: false,
           auth: {
-            user: 'timmothy.pagac@ethereal.email',
-            pass: 'FbaYAAAMT18UAgy77P'
+            user: 'judson76@ethereal.email',
+            pass: 'HQuUDwtRXwMhAeT8GT'
           }
         });
     
         const info = await transporter.sendMail({
           from: "'MMG enterprise' <freida91@ethereal.email>",
-          to: 'eduarpi2001@gmail.com',
+          to: `${forgetPasswordDTO.email}`,
           subject: 'forget password',
-          html: this.contentHtml
+          html: this.contentHtmlForget
         });
     
         console.log('Message sent', info.messageId);
       }
+
+      async sendEmailToMaxTries(email: string) {
+        const transporter = nodemailer.createTransport({
+           host: "smtp.ethereal.email",
+           port: 587,
+           secure: false,
+           auth: {
+             user: 'judson76@ethereal.email',
+             pass: 'HQuUDwtRXwMhAeT8GT'
+           }
+         });
+     
+         const info = await transporter.sendMail({
+           from: "'MMG enterprise' <freida91@ethereal.email>",
+           to: `${email}`,
+           subject: 'Intruder?',
+           html: this.contentHtmlMaxTries
+         });
+     
+         console.log('Message sent', info.messageId);
+       }
     
       async getByEmail(email: string, id: number) {
         const user = await this.userModel.findOne({ email: email });
@@ -107,17 +140,44 @@ export class AuthService {
       async verifyPassword(password: string, hashedPassword: string) {
         const isPasswordMatching = await compareSync(password, hashedPassword);
         if (!isPasswordMatching) {
-          throw new HttpException(
+          new HttpException(
             'Wrong credential provided',
             HttpStatus.BAD_REQUEST,
           );
+          return false;  
+        }
+        return true;
+      }
+      async countTries(passwordH: boolean, email: string){
+        if(passwordH){
+          throw new HttpException(
+            'User Exist',
+            HttpStatus.OK,
+          );
+        }else{
+          
+          if(count >= 5){
+            this.sendEmailToMaxTries(email)
+            throw new HttpException(
+              'Maximum number of attempts',
+              HttpStatus.BAD_REQUEST,
+            );
+          }else{
+            count++
+            throw new HttpException(
+              'Wrong credential provided',
+              HttpStatus.BAD_REQUEST,
+            );
+          }
         }
       }
-      
 
-      contentHtml = `
+      contentHtmlForget = `
     <h1><b>Change your password</b></h1>
     <a href="http://localhost:3000/api/users/password/new"><b>New Password</b></a>
     `
     
+    contentHtmlMaxTries= `
+    <h1><b>someone is trying to enter your account, Is you?</b></h1>
+    `
 }
